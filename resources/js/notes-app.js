06300f15ba3760addf43d1611e8961053da9482c
Patricio-Ra <patricio.raschetti@gmail.com>
2019-10-06 22:38:31 -0300

    done

--- resources/js/notes-app.js
+++ resources/js/notes-app.js
@@ -1,7 +1,25 @@
 'use strict'
 
+const notes = [{
+  title: 'My next trip',
+  body: 'I would like to go to Spain.'
+}, {
+  title: 'Habbits to work on',
+  body: 'Exercise. Eating a bit better.'
+}, {
+  title: 'Office modifications',
+  body: 'Get a new monitor'
+}];
+
 const ps = document.querySelectorAll('p');
 // console.log(ps);
 // console.log(typeof ps);
+// ps.remove();
 
-ps.remove();
+ps.forEach(p => p.textContent = '******');
+// console.log(p.textContent);
+
+const newP = document.createElement('p');
+newP.textContent = 'New text element from JS';
+document.querySelector('body').appendChild(newP);
+// Note that the .forEach is above this.
