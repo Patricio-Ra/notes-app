d5b81021883917c0191d2505203d5a1db1556510
Patricio-Ra <patricio.raschetti@gmail.com>
2019-10-07 23:30:37 -0300

    Filter feature

--- resources/js/notes-app.js
+++ resources/js/notes-app.js
@@ -11,15 +11,44 @@ const notes = [{
   body: 'Get a new monitor'
 }];
 
-const ps = document.querySelectorAll('p');
-// console.log(ps);
-// console.log(typeof ps);
-// ps.remove();
+const filters = {
+  searchText: ''
+}
 
-ps.forEach(p => p.textContent = '******');
-// console.log(p.textContent);
+const renderNotes = function (notes, filters) {
+  const filteredNotes = notes.filter(note => {
+    return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
+  });
+  document.querySelector('#notes').innerHTML = '';
+  filteredNotes.forEach(note => {
+    const noteElement = document.createElement('p');
+    noteElement.textContent = note.title;
+    document.querySelector('#notes').appendChild(noteElement);
+  })
+}
 
-const newP = document.createElement('p');
-newP.textContent = 'New text element from JS';
-document.querySelector('body').appendChild(newP);
-// Note that the .forEach is above this.
+renderNotes(notes, filters);
+
+document.querySelector('#create-note').addEventListener('click', e => e.target.textContent = 'The button was clicked');
+
+document.querySelector('#remove-all').addEventListener('click', e => {
+  document.querySelectorAll('.note').forEach(note => note.remove());
+});
+
+document.querySelector('#search-text').addEventListener('input', e => { filters.searchText = e.target.value;
+  renderNotes(notes, filters);
+});
+
+// const ps = document.querySelectorAll('p');
+// // console.log(ps);
+// // console.log(typeof ps);
+// // ps.remove();
+
+// ps.forEach(p => p.textContent = '******');
+// // console.log(p.textContent);
+
+// const newP = document.createElement('p');
+// newP.textContent = 'New text element from JS';
+// document.querySelector('body').appendChild(newP);
+// // Note that the .forEach is above this.
+
